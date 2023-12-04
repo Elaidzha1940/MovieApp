ab1fc077c587674423e59d98c8f2717adeb056bf
Elaidzha1940 <eli080293@gmail.com>
2023-12-04 07:46:01 +0300

    MovieApp

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Elaidzha Shchukin on 04.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
