416a1f27752effead510c2748d747d176de2f58c
Elaidzha1940 <eli080293@gmail.com>
2023-12-05 05:45:47 +0300

    Add Head

--- MovieApp/ContentView.swift
+++ MovieApp/ContentView.swift
@@ -13,11 +13,49 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-        
+            Head()
         }
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
+}
+
+struct Head: View {
+    var body: some View {
+        VStack(spacing: 15) {
+            HStack {
+                VStack(alignment: .leading, spacing: 15) {
+                    Text("Marvel")
+                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        HStack(spacing: 10) {
+                            Text("Mavel Studios Movies")
+                                .font(.system(size: 18, weight: .bold, design: .rounded))
+
+                            Image(systemName: "chevron.down")
+                                .bold()
+                        }
+                    })
+                    .foregroundColor(.white)
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "line.3.horizontal.circle")
+                        .renderingMode(.original)
+                        .foregroundColor(.white)
+                })
+            }
+        }
+        .padding()
+    }
 }
